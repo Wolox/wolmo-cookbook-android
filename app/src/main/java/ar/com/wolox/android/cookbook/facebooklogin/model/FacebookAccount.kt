dd5a993ff8d19b522910f80dc28012b1368d47f6
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-02-15 13:15:38 -0300

    Json account

--- app/src/main/java/ar/com/wolox/android/cookbook/facebooklogin/model/FacebookAccount.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/facebooklogin/model/FacebookAccount.kt
@@ -3,14 +3,11 @@ package ar.com.wolox.android.cookbook.facebooklogin.model
 import android.net.Uri
 import org.json.JSONObject
 
-class FacebookAccount(private val jsonAccount: JSONObject) {
-    val name: String
-        get() = jsonAccount.optString("name") ?: ""
-    val email: String
-        get() = jsonAccount.optString("email") ?: ""
-    val picture: Uri?
-        get() {
-            val url = jsonAccount.optJSONObject("picture")?.optJSONObject("data")?.optString("url")
-            return if (url != null) Uri.parse(url) else null
-        }
+class FacebookAccount(jsonAccount: JSONObject) {
+    val name = jsonAccount.optString("name") ?: ""
+    val email = jsonAccount.optString("email") ?: ""
+    val picture = {
+        val url = jsonAccount.optJSONObject("picture")?.optJSONObject("data")?.optString("url")
+        if (url != null) Uri.parse(url) else null
+    }
 }
