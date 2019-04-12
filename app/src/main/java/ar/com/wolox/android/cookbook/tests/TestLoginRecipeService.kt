c5ce19713dc6cec2f822876fa76fddc63573bbee
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-04-12 13:55:34 -0300

    Indentation

--- app/src/main/java/ar/com/wolox/android/cookbook/tests/TestLoginRecipeService.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/tests/TestLoginRecipeService.kt
@@ -11,20 +11,24 @@ open class TestLoginRecipeService @Inject constructor() {
      * This task should make the login against something like an API or a DB,
      * but this doesn't concern the tests so it will always returns a hardcoded user with the received email.
      */
-    private class LoginTask constructor(private val email: String,
-                                        private val password: String,
-                                        private val onSuccess: (TestLoginUserModel) -> Unit,
-                                        private val onError: () -> Unit) : AsyncTask<Void, Void, TestLoginUserModel?>() {
+    private class LoginTask constructor(
+            private val email: String,
+            private val password: String,
+            private val onSuccess: (TestLoginUserModel) -> Unit,
+            private val onError: () -> Unit
+    ) : AsyncTask<Void, Void, TestLoginUserModel?>() {
 
         override fun doInBackground(vararg params: Void?) = TestLoginUserModel(email)
 
         override fun onPostExecute(user: TestLoginUserModel?) = user?.let { onSuccess(it) } ?: onError()
     }
 
-    fun login(email: String,
-              password: String,
-              onSuccess: (TestLoginUserModel) -> Unit,
-              onError: () -> Unit) {
+    fun login(
+            email: String,
+            password: String,
+            onSuccess: (TestLoginUserModel) -> Unit,
+            onError: () -> Unit
+    ) {
         LoginTask(email, password, onSuccess, onError).execute()
     }
 }
