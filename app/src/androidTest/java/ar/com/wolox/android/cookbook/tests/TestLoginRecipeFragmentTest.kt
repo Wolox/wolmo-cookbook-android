0c411b30846056dc9f064ff28d273cf27f3196bf
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-05-06 11:11:20 -0300

    Testing with FragmentScenario

--- app/src/androidTest/java/ar/com/wolox/android/cookbook/tests/TestLoginRecipeFragmentTest.kt
+++ app/src/androidTest/java/ar/com/wolox/android/cookbook/tests/TestLoginRecipeFragmentTest.kt
@@ -1,58 +1,78 @@
 package ar.com.wolox.android.cookbook.tests
 
-import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.annotation.StringRes
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.annotation.UiThreadTest
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers.withDecorView
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ar.com.wolox.android.cookbook.R
-import org.hamcrest.CoreMatchers.not
+import ar.com.wolox.android.cookbook.datasync.DataSyncRecipeActivity
+import org.hamcrest.Matcher
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class TestLoginRecipeFragmentTest : WolmoFragmentTest<TestLoginRecipeView>(TestLoginRecipeFragment()) {
+class TestLoginRecipeFragmentTest {
 
-    private var fragment1: Class<out Fragment>? = null
+    private lateinit var scenario: FragmentScenario<TestLoginRecipeFragment>
 
     @Before
-    @UiThreadTest
     fun prepareFragment() {
-        val scenario = launchFragmentInContainer<TestLoginRecipeFragment>()
+        Intents.init()
+        scenario = launchFragmentInContainer<TestLoginRecipeFragment>()
+    }
+
+    @After
+    fun clean() {
+        Intents.release()
     }
 
     @Test
-    @UiThreadTest
     fun should_ShowErrorMessage_When_ShowEmptyEmailError() {
-        fragment.showEmptyEmailError()
+        scenario.onFragment {
+            it.showEmptyEmailError()
+        }
         onView(withId(R.id.vTestLoginEmailInput)).check(matches(hasErrorText(R.string.test_login_empty_field)))
     }
 
     @Test
-    @UiThreadTest
     fun should_ShowErrorMessage_When_ShowInvalidEmailError() {
-        fragment.showInvalidEmailError()
+        scenario.onFragment {
+            it.showInvalidEmailError()
+        }
         onView(withId(R.id.vTestLoginEmailInput)).check(matches(hasErrorText(R.string.test_login_invalid_email)))
     }
 
     @Test
-    @UiThreadTest
     fun should_ShowErrorMessage_When_ShowEmptyPasswordError() {
-        fragment.showEmptyPasswordError()
+        scenario.onFragment {
+            it.showEmptyPasswordError()
+        }
         onView(withId(R.id.vTestLoginPasswordInput)).check(matches(hasErrorText(R.string.test_login_empty_field)))
     }
 
     @Test
-    @UiThreadTest
-    fun should_ShowErrorToast_When_ShowLoginError() {
-        fragment.showLoginError()
-        onView(withText(R.string.test_login_login_error))
-                .inRoot(withDecorView(not((activity.window.decorView)))).check(matches(isDisplayed()))
+    fun should_GoToDataSyncRecipe_When_GoToNextWindow() {
+        scenario.onFragment {
+            it.goToNextWindow()
+        }
+        intended(hasComponent(DataSyncRecipeActivity::class.java.name))
+    }
+
+    private fun hasErrorText(@StringRes errorRes: Int): Matcher<View> {
+        lateinit var string: String
+        scenario.onFragment {
+            string = it.requireActivity().getString(errorRes)
+        }
+        return ViewMatchers.hasErrorText(string)
     }
 }
