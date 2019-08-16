d8bf140f07b376f7c4a43c1d3c8f75ebc7a3fc41
Federico Sanucci <federico.sanucci@wolox.com.ar>
2019-08-16 09:09:44 -0300

    Steps to register app in twitterApi

--- app/src/main/java/ar/com/wolox/android/cookbook/twitterlogin/TwitterLoginRecipeActivity.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/twitterlogin/TwitterLoginRecipeActivity.kt
@@ -4,6 +4,14 @@ import android.content.Intent
 import ar.com.wolox.android.cookbook.R
 import ar.com.wolox.wolmo.core.activity.WolmoActivity
 
+/**
+ * To connect app with TwitterApi:
+ * 1. Register app in "https://developer.twitter.com/en/apps"
+ * 2. Required fields (to obtain all data from api, like user email):
+ * "Callback URL" (complete with "twittersdk://", "Terms of service URL" and "Privacy policy URL"
+ * 3. App permissions: Enable "Request email address"
+ * 4. Save "API Key" and "API Secret Key" from keys and tokens view
+ */
 class TwitterLoginRecipeActivity : WolmoActivity() {
 
     override fun layout(): Int = R.layout.activity_base
