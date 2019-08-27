5ee9a8a543c76faae3988ec80a85d760ac1345f4
FedericoSanucciWX <federico.sanucci@wolox.com.ar>
2019-08-27 15:13:10 -0300

    exportShema in db manager

--- app/src/main/java/ar/com/wolox/android/cookbook/room/database/RoomDatabaseManager.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/room/database/RoomDatabaseManager.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [RoomDataEntity::class], version = 1)
+@Database(entities = [RoomDataEntity::class], version = 1, exportSchema = false)
 abstract class RoomDatabaseManager : RoomDatabase() {
 
     abstract fun RoomDataDao(): RoomDataDao
