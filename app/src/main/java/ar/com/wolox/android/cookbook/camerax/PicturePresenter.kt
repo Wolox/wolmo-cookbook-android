be82694318ac26dc88d288c09159df6f284fadc8
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-09-02 12:04:04 -0300

    Deleting image on close

--- app/src/main/java/ar/com/wolox/android/cookbook/camerax/PicturePresenter.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/camerax/PicturePresenter.kt
@@ -1,13 +1,22 @@
 package ar.com.wolox.android.cookbook.camerax
 
 import ar.com.wolox.wolmo.core.presenter.BasePresenter
+import java.io.File
 import javax.inject.Inject
 
 class PicturePresenter @Inject constructor() : BasePresenter<PictureView>() {
 
+    lateinit var image: String
+
     /** Invoked on view init with the [image] requested to show. */
-    fun onInit(image: String) = view.showImage(image)
+    fun onInit(image: String) {
+        this.image = image
+        view.showImage(image)
+    }
 
     /** Invoked on close button clicked. */
-    fun onCloseButtonClicked() = view.finish()
+    fun onCloseButtonClicked() {
+        File(image).delete()
+        view.finish()
+    }
 }
