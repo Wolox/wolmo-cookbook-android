43e034bf79744abea21c50dd9fb05059d1a18778
FedericoSanucciWX <federico.sanucci@wolox.com.ar>
2019-09-02 17:39:10 -0300

    Whitespaces in methods

--- app/src/main/java/ar/com/wolox/android/cookbook/room/RoomRecipeView.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/room/RoomRecipeView.kt
@@ -5,12 +5,18 @@ import ar.com.wolox.android.cookbook.room.database.RoomDataEntity
 interface RoomRecipeView {
 
     fun loginSuccess()
+
     fun loginError()
+
     fun logout()
 
     fun updateEntities(entities: List<RoomDataEntity>)
+
     fun insertEntity(entity: RoomDataEntity)
+
     fun clearEntities()
+
     fun deleteEntity(entity: RoomDataEntity)
+
     fun modifyEntity(entity: RoomDataEntity)
 }
