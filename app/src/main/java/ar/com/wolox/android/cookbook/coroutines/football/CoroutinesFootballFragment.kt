8a6f6319c199768a40c6139891d69713d9e2aa7d
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-10-12 15:50:16 -0300

    Rename

--- app/src/main/java/ar/com/wolox/android/cookbook/coroutines/football/CoroutinesFootballFragment.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/coroutines/football/CoroutinesFootballFragment.kt
@@ -5,7 +5,7 @@ import ar.com.wolox.android.cookbook.coroutines.core.fromHtml
 import ar.com.wolox.android.cookbook.coroutines.football.model.Competition
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import ar.com.wolox.wolmo.core.util.ToastFactory
-import kotlinx.android.synthetic.main.fragment_coroutines_example.*
+import kotlinx.android.synthetic.main.fragment_coroutines_football.*
 import javax.inject.Inject
 
 class CoroutinesFootballFragment : WolmoFragment<CoroutinesFootballPresenter>(), CoroutinesFootballView {
@@ -13,7 +13,7 @@ class CoroutinesFootballFragment : WolmoFragment<CoroutinesFootballPresenter>(),
     @Inject
     internal lateinit var toastFactory: ToastFactory
 
-    override fun layout() = R.layout.fragment_coroutines_example
+    override fun layout() = R.layout.fragment_coroutines_football
 
     override fun init() {}
 
