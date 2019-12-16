a307fefdfb29090c1989040c375b6bf1466404f0
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-12-16 13:13:24 -0300

    Reverting some lines in keystore.sh

--- scripts/keystore.sh
+++ scripts/keystore.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ -f app/keystore.gradle ]; then
-    echo "/Users/facundomontesanto/git/wolmo-cookbook-android/app/keystore.gradle file already exists!"
+    echo "app/keystore.gradle file already exists!"
     exit 1
 fi
-cat > /Users/facundomontesanto/git/wolmo-cookbook-android/app/keystore.gradle << EOF
+cat > app/keystore.gradle << EOF
 ext.release_keystore=file('keystore/debug.keystore')
 ext.key_alias='androiddebugkey'
 ext.key_password='android'
