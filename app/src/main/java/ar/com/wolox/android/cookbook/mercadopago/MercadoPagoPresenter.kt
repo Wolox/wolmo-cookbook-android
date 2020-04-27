a0e57af957f5a7bc795fcb3d2794e4331f250ef3
rickmeli <ext_rmarquez@mercadolibre.com>
2020-04-27 17:35:28 -0300

    Moving public_key to build config

--- app/src/main/java/ar/com/wolox/android/cookbook/mercadopago/MercadoPagoPresenter.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/mercadopago/MercadoPagoPresenter.kt
@@ -1,17 +1,22 @@
 package ar.com.wolox.android.cookbook.mercadopago
 
+import android.content.Context
+import ar.com.wolox.android.cookbook.R
 import ar.com.wolox.wolmo.core.presenter.BasePresenter
 import com.mercadopago.android.px.core.MercadoPagoCheckout
 import javax.inject.Inject
 
-class MercadoPagoPresenter @Inject constructor() : BasePresenter<MercadoPagoView>() {
+class MercadoPagoPresenter @Inject constructor(val context: Context) : BasePresenter<MercadoPagoView>() {
 
     fun onPayButtonClicked() {
-        view?.payProduct(MercadoPagoCheckout.Builder(PUBLIC_KEY, DUMMY_PREFERENCE_ID).build())
+        view?.payProduct(MercadoPagoCheckout.Builder(
+                context.getString(PUBLIC_KEY),
+                DUMMY_PREFERENCE_ID)
+                .build())
     }
 
     companion object {
-        private const val PUBLIC_KEY = "YOUR_PUBLIC_KEY_HERE"
+        private const val PUBLIC_KEY = R.string.mercado_pago_public_key
 
         private const val DUMMY_PREFERENCE_ID = "243962506-0bb62e22-5c7b-425e-a0a6-c22d0f4758a9"
     }
