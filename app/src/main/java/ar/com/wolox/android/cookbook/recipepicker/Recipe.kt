43f1eba7a75879feed14f8baa25d4a0d2521c447
Federico Fasitella <50247351+ffasitella@users.noreply.github.com>
2021-12-10 15:45:44 -0300

    Cookbook redesign (#41)
    * Added Databinding to project
    
    * Started new cookbook design
    
    * Finished new design
    
    * Replaced old icons with new ones. Changed app name
    
    * Solved hardcoded dimen

--- app/src/main/java/ar/com/wolox/android/cookbook/recipepicker/Recipe.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/recipepicker/Recipe.kt
@@ -4,22 +4,22 @@ package ar.com.wolox.android.cookbook.recipepicker
  * Every instance of [Recipe] will be one of the options the users of the Cookbook will be able
  * to select. To add a [Recipe], just create a new instance under this enum.
  */
-enum class Recipe {
-    LOTTIE,
-    MERCADOPAGO,
-    ANALYTICS,
-    COROUTINES,
-    GOOGLE_LOGIN,
-    FACEBOOK_LOGIN,
-    TWITTER_LOGIN,
-    INSTAGRAM_LOGIN,
-    ROOM,
-    MP_CHART,
-    NAVIGATION,
-    DATA_SYNC,
-    TESTS,
-    KOIN,
-    NOTIFICATIONS,
-    GRAPH_QL,
-    BIOMETRIC_LOGIN
+enum class Recipe(val fullName: String) {
+    LOTTIE("Lottie"),
+    MERCADOPAGO("Mercado Pago"),
+    ANALYTICS("Firebase Analytics"),
+    COROUTINES("Coroutines"),
+    GOOGLE_LOGIN("Google login"),
+    FACEBOOK_LOGIN("Facebook login"),
+    TWITTER_LOGIN("Twitter login"),
+    INSTAGRAM_LOGIN("Instagram login"),
+    ROOM("RoomDB"),
+    MP_CHART("MP Charts"),
+    NAVIGATION("Navigation architecture component"),
+    DATA_SYNC("Data Sync Recipe with a Pokemon flavor"),
+    TESTS("Tests"),
+    KOIN("Koin"),
+    NOTIFICATIONS("Push Notifications"),
+    GRAPH_QL("GraphQL"),
+    BIOMETRIC_LOGIN("Login with fingerprint")
 }
--- app/src/main/java/ar/com/wolox/android/cookbook/recipepicker/RecipeItem.kt
+++ app/src/main/java/ar/com/wolox/android/cookbook/recipepicker/RecipeItem.kt
@@ -1,10 +1,13 @@
 package ar.com.wolox.android.cookbook.recipepicker
 
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import ar.com.wolox.android.cookbook.R
 
 data class RecipeItem(
     val recipe: Recipe,
     @DrawableRes val imageResId: Int,
-    @StringRes val stringResId: Int
+    @StringRes val stringResId: Int,
+    @ColorRes val backgroundResId: Int = R.color.px_white
 )
